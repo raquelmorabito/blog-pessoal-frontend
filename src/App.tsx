76a62bb74913aa619847526e362270129baf0b7d
User <raquelmorabito@hotmail.com>
2025-01-22 18:14:30 -0300

    Introdução ao Axios, criação das Models e Context API, integração do AuthContext e ajuste da lógica de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import{ AuthProviderr } from './contexts/AuthContext'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
@@ -9,6 +10,7 @@ import Cadastro from './pages/cadastro/Cadastro'
 function App() {
   return (
     <>
+        <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -21,6 +23,7 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
+        </AuthProvider>
     </>
   )
 }
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,52 +1,39 @@
-import './Login.css';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { UserContext } from '../../contexts/UserContext';
 
 function Login() {
+  const { nome, trocarNome } = useContext(UserContext);
+  let navigate = useNavigate()
 
-    return (
-        <>
-            <div className="grid grid-cols-1 lg:grid-cols-2 h-screen place-items-center font-bold ">
-                <form className="flex justify-center items-center flex-col w-1/2 gap-4" >
-                    <h2 className="text-slate-900 text-5xl ">Entrar</h2>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="usuario">Usuário</label>
-                        <input
-                            type="text"
-                            id="usuario"
-                            name="usuario"
-                            placeholder="Usuario"
-                            className="border-2 border-slate-700 rounded p-2"
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    navigate('/home')
+  }
 
-                        />
-                    </div>
-                    <div className="flex flex-col w-full">
-                        <label htmlFor="senha">Senha</label>
-                        <input
-                            type="password"
-                            id="senha"
-                            name="senha"
-                            placeholder="Senha"
-                            className="border-2 border-slate-700 rounded p-2"
+  return (
+    <div className='container'>
+      <form onSubmit={handleSubmit} className="formulario">
+        <h1>Autenticar Usuário</h1>
+        <div className="campo">
+          <label htmlFor="usuario">Nome</label>
+          <input
+            type="text"
+            id="usuario"
+            name="usuario"
+            placeholder="Nome do Usuário"
+            value={nome}
+            onChange={(event) => trocarNome(event.target.value)}
+          />
+        </div>
+        <button type='submit'>
+          Entrar
+        </button>
 
-                        />
-                    </div>
-                    <button 
-                        type='submit' 
-                        className="rounded bg-indigo-400 flex justify-center
-                                   hover:bg-indigo-900 text-white w-1/2 py-2">
-                        <span>Entrar</span>
-                    </button>
+      </form>
 
-                    <hr className="border-slate-800 w-full" />
-
-                    <p>
-                        Ainda não tem uma conta?{' '}
-                        Cadastre-se
-                    </p>
-                </form>
-                <div className="fundoLogin hidden lg:block"></div>
-            </div>
-        </>
-    );
+    </div>
+  );
 }
 
 export default Login;
