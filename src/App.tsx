7620ebfd9ed1a55825bb03ec4ea5fa694363afc4
User <raquelmorabito@hotmail.com>
2025-01-22 18:59:24 -0300

    Implementação da lógica do Login, atualização do Navbar e testes realizados

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import './App.css'
-import Footer from './components/footer/Footer'
-import Navbar from './components/navbar/Navbar'
-import{ AuthProviderr } from './contexts/AuthContext'
-import Home from './pages/home/Home'
-import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import { AuthProvider } from './contexts/AuthContext'; // Correção aqui
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Cadastro from './pages/cadastro/Cadastro';
 
 function App() {
   return (
     <>
-        <AuthProvider>
+      <AuthProvider>
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -23,9 +23,9 @@ function App() {
           </div>
           <Footer />
         </BrowserRouter>
-        </AuthProvider>
+      </AuthProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
